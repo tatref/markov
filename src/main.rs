f1449d9b8cc750e0f4454ffec00b7fd5ac636037
tatref <tatref@github.com>
2017-10-24 02:29:56 +0200

    better phrase terminators

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ mod markov {
         /// Feed the Markov chain some raw text
         /// Will split phrases on "."
 		pub fn feed_text(&mut self, text: &str) {
-			let phrases = text.split(".");
+			let phrases = text.split(|c| c == '.' || c == '!' || c == '?');
 
 			for phrase in phrases {
                 let words: Vec<Token> = Markov::get_tokens(phrase.trim());
@@ -138,7 +138,7 @@ mod markov {
 			match self.h.get(&prefix) {
 				None => None,  //TODO: maybe Some(Token::End)
 				Some(ref predictions) => {
-                    println!("prefix={:?}  => {:?}", prefix, predictions);
+                    //println!("prefix={:?}  => {:?}", prefix, predictions);
 
                     let mut choices: Vec<Weighted<Token>> = predictions.iter()
                         .map(|(word, proba)| Weighted
@@ -161,12 +161,32 @@ fn main() {
 	use markov::Markov;
     //use markov::Token;
 
-	let mut markov = Markov::new(3);
+	let mut markov = Markov::new(2);
 
-	let text = "Ceci est une phrase. Ceci est un avion.\
-    Ceci est une pipe. Ceci n'est pas une pipe";
+	let feed = "
+    La société russe Kaspersky, l’un des principaux éditeurs de logiciels antivirus au monde, a été accusée la semaine dernière dans plusieurs articles de la presse américaine d’avoir été utilisée pour espionner les agences de renseignement américaines. Entretien avec son fondateur, Eugène Kaspersky, qui nie toute collusion avec les services de renseignement russe.    
 
-	markov.feed_text(text);
+Je ne le pense pas, notre entreprise n’est pas assez grande pour être utilisée dans ces turbulences politiques. Il y a sans doute plusieurs raisons derrière ces accusations, et peut-être que la situation politique est l’une d’entre elles.
+Nous n’avons aucune coopération secrète avec quelque agence de renseignement que ce soit dans le monde, ni en Russie ni ailleurs. Nous n’aidons aucun pays à faire de l’espionnage. Lors de notre recherche de fichiers malveillants [sur un ordinateur], en cas de détection de programmes dangereux qui viennent de faire leur apparition sur Internet, nous les envoyons dans le cloud [sur les serveurs de Kaspersky] si notre produit est connecté à ce service, et seulement dans ce cas. Si notre produit n’est pas connecté au cloud que nous fournissons, nous ne voyons rien.
+
+La plupart d’entre eux sont analysés automatiquement et stockés dans nos archives. Dans certains cas, ces données arrivent dans les mains de nos chercheurs. Et ces experts ont des instructions très strictes : s’il y a la moindre information classifiée, de quelque pays que ce soit, qui a été envoyée dans le cloud, cette information est supprimée.
+Les programmes malveillants ne peuvent pas être classifiés.
+Je l’ai dit à de nombreuses reprises : mon entreprise est ouverte à un audit, pas seulement au code source, mais aussi aux mises à jour que nous faisons. Nous avons des requêtes de certains pays, et nous y accédons. Nous l’avons fait récemment au Brésil, en août. Ils sont venus dans nos locaux et ont inspecté notre code. Toutes les portes étaient ouvertes.
+
+Nous travaillons activement sur un transparency center qui sera basé dans un pays d’Europe occidentale. Nous allons y mettre à disposition notre code source et nos bases de données pour inspection. Tout gouvernement ou toute organisation respectée qui auront une question sur nos produits seront les bienvenus. Ce centre prouvera que tout est en ordre dans nos produits, technologies, mises à jour, services.
+Il se trouvera dans un des pays européens qui ont la législation la plus stricte en matière de données personnelles. Pour être honnête, le gagnant est votre voisin de l’Est ; l’Allemagne est bien connue pour sa législation en matière de données personnelles.
+L’audition aura lieu après-demain [mercredi 25 octobre], mais nous ne sommes pas invités. Ils ne nous ont pas dit pourquoi, nous n’avons aucune information ; je ne comprends pas ce qui se passe. De grands médias américains ont publié de fausses accusations contre notre entreprise, sans aucune preuve. Cette attaque est disproportionnée par rapport à notre taille : notre part de marché n’est pas si importante, nous sommes juste une entreprise de sécurité informatique. Je ne comprends pas pourquoi on parle tant de mon entreprise, et surtout pourquoi maintenant.
+Si je suis invité, j’irai.
+Nous sommes très intéressés par tout fait, toute preuve, qui étaierait les accusations qui sont portées contre Kaspersky. Je suis sûr, presque à cent pour cent, qu’il n’y a aucun problème de sécurité dans mon entreprise : nous faisons des audits de sécurité, à la fois pour nos logiciels et nos mises à jour, et nous n’avons détecté aucun problème. Mais s’il y a un problème, je veux en discuter. Je ne veux pas qu’un pays soit mécontent de mon entreprise et de ses produits. Nous protégeons nos clients contre les cybermenaces, et nous voulons que tous les gouvernements, toutes les entreprises comprennent cela. Nous sommes ouverts à la discussion et aux questions. Posez-les-nous ! Montrez-nous des preuves de ce qui ne va pas avec Kaspersky.
+Chaque année, nous auditons notre réseau, nous simulons des attaques. Désormais, il est impossible de pénétrer notre réseau sans être détecté. 
+Nous n’avons ce genre de problème qu’aux Etats-Unis. Nous nous attendons à une croissance négative dans ce pays, mais dans le reste du monde nous sommes en croissance. Cette tension avec les Etats-Unis n’affecte pas notre activité.
+Je ne veux pas m’approfondir sur les scénarios possibles. Je ne sais pas, mais la question c’est pourquoi maintenant ? Et pourquoi pas avant ? Nous travaillons sur les attaques étatiques depuis des années, d’abord il y a eu Stuxnet [logiciel malveillant ayant visé le programme nucléaire iranien], puis Duqu [autre programme lié à Stuxnet], et beaucoup d’autres, depuis près de dix ans. Nous protégeons nos clients contre tous les types d’attaques étatiques.
+
+
+
+    ";
+
+	markov.feed_text(feed);
 
     println!("\ncontent:");
 	for val in markov.h.iter() {
